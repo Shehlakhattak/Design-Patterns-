bebe2301736ae4b6c87ca337eeb78ab641df8a29
Shehlakhattak <92219029+Shehlakhattak@users.noreply.github.com>
2021-12-24 07:34:50 -0800

    Add files via upload

---
+++ visitor.guru/src/visitor/guru/Visitor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visitor.guru;
+
+/**
+ *
+ * @author shah
+ */
+
+
+public interface Visitor {
+    String visitDot(Dot dot);
+
+    String visitCircle(Circle circle);
+
+    String visitRectangle(Rectangle rectangle);
+
+    String visitCompoundGraphic(CompoundShape cg);
+
+}
